f8c8dc7e959dbf9c0745ede78fad283c7b03e1ab
Shohboz Xurramov <shohboztuit@gmail.com>
2023-09-26 17:13:11 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2, b=3;
+            Console.WriteLine($"{a} : {b}");
+            Console.WriteLine();
+
+            Swapper<int>.Swap(ref a, ref b );
+            Console.WriteLine($"{a} : {b}");
+
+
+
+
+        }
+    }
+    class Swapper<T>
+    {
+        public static void Swap(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+    }
+    
+}
